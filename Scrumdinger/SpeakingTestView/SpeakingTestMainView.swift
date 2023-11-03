0bd223c46d929ecbd59be00b04e48791f5b017a7
YuanlaiHe <yuanlaihe@126.com>
2023-11-03 03:34:02 -0400

    new version 001 appending

---
+++ Scrumdinger/SpeakingTestView/SpeakingTestMainView.swift
@@ -0,0 +1,303 @@
+//
+//  SpeakingTestMainView.swift
+//  Scrumdinger
+//
+//  Created by YuanlaiHe on 2023-11-02.
+//
+
+import SwiftUI
+import AVFoundation
+
+struct TitleView: View {
+    let topic: String
+    var body: some View {
+        Text(topic)
+            .font(.title)
+            .foregroundColor(.black)
+    }
+}
+
+struct ReadingPracticeView: View {
+    let textToRead: String // The text that the user will read aloud
+    @Binding var isRecording: Bool
+    private let maxHeight: CGFloat = 500 // Maximum height for the scroll view
+    
+    var body: some View {
+        ZStack(alignment: .bottom) {
+            ScrollView {
+                Text(textToRead)
+                    .padding()
+                    .foregroundColor(.primary)
+            }
+            .frame(maxHeight: maxHeight)
+            ScrollIndicatorView()
+                .padding(.top, 8)
+                .padding(.bottom, 8)
+        }
+        .overlay(
+            RoundedRectangle(cornerRadius: 12)
+                .stroke(isRecording ? Color.red : Color.secondary, lineWidth: 2)
+        )
+    }
+}
+struct ScrollIndicatorView: View {
+    var body: some View {
+        HStack {
+            Image(systemName: "arrow.down")
+            Text("Scroll to see more")
+        }
+        .padding(4)
+        .font(.caption)
+        .foregroundColor(.white)
+        .background(Color.black.opacity(0.5)) // 半透明黑色背景
+        .cornerRadius(10)
+    }
+}
+
+
+
+
+struct SpeakingTestMainView: View {
+    @Environment(\.scenePhase) private var scenePhase
+    @Binding var englishPracticeTopics: [EnglishPracticeTopic]
+    @StateObject var speakingTimer = EnglishSpeakingTimer()
+    @StateObject var speechRecognizer = SpeechRecognizer()
+    @State private var isRecording = false
+    @State private var recordingState: RecordingState = .readyToRecord
+    @State private var showingHistory = false
+    @State private var showingTopicInfo = false
+    @State private var addingNewTopic = false
+    private var player: AVPlayer { AVPlayer.sharedDingPlayer }
+    let saveAction: ()->Void
+    
+    var body: some View {
+        NavigationView {
+        ZStack {
+            RoundedRectangle(cornerRadius: 16.0)
+                .fill(englishPracticeTopics[0].theme.mainColor)
+            VStack {
+//                TitleView(topic: "The Great Wall")
+//                    .padding(.top, 8)
+                MeetingHeaderView(secondsElapsed: speakingTimer.secondsElapsed, secondsRemaining: speakingTimer.secondsRemaining, theme: englishPracticeTopics[0].theme)
+                    .padding(.bottom, 16)
+                ReadingPracticeView(textToRead: englishPracticeTopics[0].topicContent, isRecording: $isRecording)
+                    .padding(.horizontal,8)
+                RecordingButtonView(
+                    recordingState: $recordingState,
+                    onStartRecording: {
+                        startRecording()
+                    },
+                    onStopRecording: {
+                        stopRecording()
+                    },
+                    onPlay:{
+                        playRecording()
+                    },
+                    onStopPlaying:{
+                        stopPlaying()
+                    },
+                    onReRecord: {
+                        startRecording() // 重新录音
+                    },
+                    onUpload: {
+                        uploadRecording()
+                    }
+                )
+                Spacer()
+            }
+        }
+        .padding(.horizontal, 8)
+        .padding(.bottom, 8)
+        .foregroundColor(englishPracticeTopics[0].theme.accentColor)
+        .navigationBarTitleDisplayMode(.inline)
+        .toolbar {
+            ToolbarItemGroup(placement: .navigationBarLeading) {
+                Button(action: {
+                    showingHistory.toggle()
+                }) {
+                    Label("History", systemImage: "clock")
+                }
+            }
+            ToolbarItemGroup(placement: .navigationBarTrailing) {
+                Button(action: {
+                    showingTopicInfo.toggle()
+                }) {
+                    Label("Info", systemImage: "info.circle")
+                }
+                Button(action: {
+                    addingNewTopic.toggle()
+                }) {
+                    Label("Add", systemImage: "plus")
+                }
+            }
+            ToolbarItem(placement: .principal) {
+                Text(englishPracticeTopics[0].topic)
+                    .font(.title)
+                    .foregroundColor(.black)
+            }
+        }
+        .sheet(isPresented: $showingHistory) {
+            // 历史视图内容
+            TopicsView(topics: $englishPracticeTopics) {
+                saveAction()
+            }
+        }
+        .sheet(isPresented: $showingTopicInfo) {
+            DetailView(topic: $englishPracticeTopics[0])
+//            NewTopicSheet(topics: $englishPracticeTopics, isPresentingNewScrumView: $showingTopicInfo)
+        }
+        .sheet(isPresented: $addingNewTopic) {
+            NewTopicSheet(topics: $englishPracticeTopics, isPresentingNewScrumView: $addingNewTopic)
+        }
+        }
+        .onDisappear {
+            endScrum()
+        }
+        .onChange(of: scenePhase) { phase in
+            if phase == .inactive { saveAction() }
+        }
+    }
+    
+    private func startScrum() {
+        speakingTimer.reset(lengthInMinutes: englishPracticeTopics[0].lengthInMinutes)
+        speechRecognizer.resetTranscript()
+        speechRecognizer.startTranscribing()
+        isRecording = true
+        speakingTimer.startSpeaking()
+    }
+    
+    private func endScrum() {
+        speakingTimer.stopSpeaking()
+        speechRecognizer.stopTranscribing()
+        isRecording = false
+        let newHistory = RecordingHistory(transcript: speechRecognizer.transcript, audioFilePathURL: speechRecognizer.audioFilePathURL)
+        englishPracticeTopics[0].recordeHistory.insert(newHistory, at: 0)
+    }
+    
+    private func startRecording() {
+        // ... 开始录音的逻辑
+        startScrum()
+        recordingState = .recording
+    }
+    
+    private func stopRecording() {
+        // ... 停止录音的逻辑
+        endScrum()
+        recordingState = .finishedRecording
+    }
+    private func playRecording() {
+        recordingState = .playing
+        // 启动播放录音的逻辑
+        playAudio()
+    }
+    
+    private func stopPlaying() {
+        recordingState = .finishedRecording
+        // 停止播放录音的逻辑
+    }
+    
+    private func uploadRecording() {
+        // ... 上传录音的逻辑
+        recordingState = .uploading
+        // 假设上传是一个异步的过程，这里是模拟上传完成后的回调
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            recordingState = .uploaded
+        }
+    }
+    
+    private func playAudio() {
+        do {
+            guard let audioFileURL = englishPracticeTopics[0].recordeHistory[0].audioFilePathURL else {
+                return
+            }
+            var audioPlayer: AVAudioPlayer?
+            audioPlayer = try AVAudioPlayer(contentsOf: audioFileURL)
+            audioPlayer?.play()
+        } catch {
+            // Handle the error of audio playback
+        }
+    }
+}
+
+struct SpeakingTestMainView_Previews: PreviewProvider {
+    static var previews: some View {
+        SpeakingTestMainView(englishPracticeTopics: .constant(EnglishPracticeTopic.sampleData), saveAction: {})
+    }
+}
+
+
+
+enum RecordingState {
+    case readyToRecord
+    case recording
+    case finishedRecording
+    case playing
+    case uploading
+    case uploaded
+}
+
+struct RecordingButtonView: View {
+    @Binding var recordingState: RecordingState
+    var onStartRecording: () -> Void
+    var onStopRecording: () -> Void
+    var onPlay: () -> Void
+    var onStopPlaying: () -> Void
+    var onReRecord: () -> Void
+    var onUpload: () -> Void
+    
+    var body: some View {
+        VStack {
+            switch recordingState {
+            case .readyToRecord, .finishedRecording:
+                Button(action: {
+                    if recordingState == .readyToRecord {
+                        onStartRecording()
+                    } else {
+                        onReRecord()
+                    }
+                }) {
+                    labelForButton(recordingState)
+                }
+                .buttonStyle(.borderedProminent)
+                
+            case .recording:
+                Button("Stop Recording", action: onStopRecording)
+                    .buttonStyle(.borderedProminent)
+                
+            case .playing:
+                Button("Stop Playback", action: onStopPlaying)
+                    .buttonStyle(.borderedProminent)
+                
+            case .uploaded, .uploading:
+                // 在上传时不显示按钮或显示上传状态
+                if recordingState == .uploading {
+                    Text("Uploading...")
+                } else {
+                    Button("Re-record", action: onReRecord)
+                        .buttonStyle(.bordered)
+                }
+            }
+            
+            // 如果已经完成录音，显示上传和播放按钮
+            if recordingState == .finishedRecording {
+                Button("Upload and Get Feedback", action: onUpload)
+                    .buttonStyle(.bordered)
+                
+                Button("Play Recording", action: onPlay)
+                    .buttonStyle(.bordered)
+            }
+        }
+        .padding()
+    }
+    
+    private func labelForButton(_ state: RecordingState) -> Text {
+        switch state {
+        case .readyToRecord:
+            return Text("Start Recording")
+        case .finishedRecording:
+            return Text("Re-record")
+        default:
+            return Text("") // 其他状态不显示这个按钮
+        }
+    }
+}
